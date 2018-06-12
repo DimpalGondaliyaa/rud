817c2efc64bee98f67ed6071edf530ef489052b1
DimpalGondaliyaa <dimpalgondaliya@hariskyline.com>
2018-06-12 10:14:13 +0530

    hhhh

--- application/views/html_document.php
+++ application/views/html_document.php
@@ -5,25 +5,33 @@
 
 	<div class="main-menu">
 
-		 <nav class="contactnav">
+		  <nav class="contactnav">
 		    <div class="nav-wrapper connavwapp">
 		      <ul id="nav-mobile" class="hide-on-med-and-down">
 		        <li><a href="<?php echo base_url(); ?>Add_Contact"><img class="iconstyle" src="<?php echo base_url() ?>html/images/accept.png">save document</a></li>
-		         <li><a href="<?php echo base_url(); ?>Add_Contact"><img class="iconstyle" src="<?php echo base_url() ?>html/images/eye.png">preview document</a></li>
-		          <li><a href="<?php echo base_url(); ?>Add_Contact"><img class="iconstyle" src="<?php echo base_url() ?>html/images/bug.png">debug html</a></li>
+		        <li><a href="<?php echo base_url(); ?>Add_Contact"><img class="iconstyle" src="<?php echo base_url() ?>html/images/eye.png">preview document</a></li>
+		        <li><a href="<?php echo base_url(); ?>Add_Contact"><img class="iconstyle" src="<?php echo base_url() ?>html/images/bug.png">debug html</a></li>
 		    </div>
 		  </nav>
 
 		<div class="row">
-			<div class="col s12 m3 textlab">
-				 <label for="first_name">Search content</label>
-				  <input  id="first_name" type="text" class="validate">
-		    </div>
+			 <div class="input-field col s6 m3">
+		          <input placeholder="Placeholder" id="first_name" type="text" class="validate">
+		          <label for="first_name">First Name</label>
+		        </div>
 		    <div class="col s1 m1"></div>
 			<div class="col s12 m8 textlab">
-				<div class="content">
-					Featured Pages
-				</div>
+					<head>
+					  <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/ckeditor/4.3.2/ckeditor.js" ></script>
+					</head>
+
+					<body>
+						<div class="testtitle">template body</div>
+					<textarea cols="80" rows="10" id="ckeExample">
+					  This is not
+					</textarea>
+					  <input type="button" onclick="javascript:fnConsolePrint();" value="Console" />
+					</body>
 			</div>
 		</div>	
 	</div>
