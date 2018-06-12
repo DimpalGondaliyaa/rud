d6db4d2f7bdc022f3a406f9fc8696f8e36bfda4e
DimpalGondaliyaa <dimpalgondaliya@hariskyline.com>
2018-06-12 15:15:52 +0530

    gf

---
+++ application/views/e_marketing.php
@@ -0,0 +1,16 @@
+<div class="main-box">
+	<div class="cmdtitle">
+		<a href="<?php echo base_url(); ?>E_marketing">Email Marketing</a>
+	</div>
+
+	<div class="main-menu">
+		  <nav class="contactnav">
+		    <div class="nav-wrapper connavwapp">
+		      <ul id="nav-mobile" class="hide-on-med-and-down">
+		        <li><a href="<?php echo base_url(); ?>Add_Contact"><img class="iconstyle" src="<?php echo base_url() ?>html/images/add.png">Schedule A New Campaign</a></li>
+		        <li><a href="<?php echo base_url(); ?>Add_Contact"><img class="iconstyle" src="<?php echo base_url() ?>html/images/icon-event.jpg">Create Email Template</a></li>
+		        <li><a href="<?php echo base_url(); ?>Add_Contact"><img class="iconstyle" src="<?php echo base_url() ?>html/images/icon-settings.jpg">Setup Senders</a></li>
+		    </div>
+		  </nav>
+	</div>
+</div>
